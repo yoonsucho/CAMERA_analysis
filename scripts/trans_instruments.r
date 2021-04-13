1d7aebde5b060f3780356744f0de506e4957da79
Gibran Hemani <explodecomputer@gmail.com>
2021-04-13 23:35:44 +0100

    analysis method

--- scripts/trans_instruments.r
+++ scripts/trans_instruments.r
@@ -5,6 +5,153 @@ library(susieR)
 library(ggplot2)
 library(dplyr)
 
+MultiAncestrySummarySet <- R6::R6Class("MultiAncestrySummarySet", list(
+  output = list(),
+  exposure_ids=NULL,
+  outcome_ids=NULL,
+  pops=NULL,
+  bfiles=NULL,
+  plink=NULL,
+  raw_instruments=NULL,
+  instrument_regions = NULL,
+  ld_matrices = NULL,
+  susie_results = NULL,
+  paintor_results = NULL,
+  expected_replications = NULL,
+
+
+
+  # Methods
+
+  initialize = function(exposure_ids, outcome_ids, pops=NULL, bfiles=NULL, plink=NULL)
+  {
+  	self$exposure_ids <- exposure_ids
+  	self$outcome_ids <- outcome_ids
+  	self$pops <- pops
+  	self$bfiles <- bfiles
+  	self$plink <- plink
+  },
+
+  # e.g. could just use mv_extract_exposures
+  # - for each exposure_id it identifies the instruments
+  # - then it tries to make these independent
+  # - then looks up the same set of instruments in all exposures
+  extract_instruments = function(exposure_ids=self$exposure_ids, ...) {
+  	self$raw_instruments <- mv_extract_exposures(exposure_ids, ...)
+  },
+
+  # Here the idea is that pop1 and pop2 might share an instrument, but the tophit for pop1 is not the causal variant
+  # Hence, in pop1 it is in LD with the causal variant but not in pop2
+  # So we extract a region around each instrument (e.g. 50kb)
+  # Search for a SNP that is best associated in both pop1 and pop2
+  extract_instrument_regions = function(raw_instruments=self$raw_instruments, bfiles=self$bfiles, )
+  {
+	
+	ldflag <- !(is.null(bfiles) & is.null(pops))
+
+	if(!is.null(bfiles))
+	{
+		stopifnot(length(bfiles) == length(ids))
+	}
+	if(!is.null(pops))
+	{
+		stopifnot(length(pops) == length(ids))
+	}
+	r <- paste0(chr, ":", max(0, position-radius), "-", position+radius)
+	message("Analysing ", r)
+
+	a <- lapply(1:length(ids), function(i)
+	{
+		message("Extracting summary data and LD for ", ids[i])
+		a1 <- ieugwasr::associations(r, ids[i]) %>%
+			dplyr::arrange(position) %>%
+			dplyr::filter(!duplicated(rsid)) %>%
+			gwasglue::ieugwasr_to_TwoSampleMR(.)
+		if(ldflag)
+		{
+			a1 <- TwoSampleMR::harmonise_ld_dat(a1, suppressMessages(suppressWarnings(ieugwasr::ld_matrix(a1$SNP, pop=pops[i], plink=plink, bfile=bfiles[i], with_alleles=TRUE))))
+			return(a1)
+		} else {
+			return(list(x=a1))
+		}
+	})
+
+	snps <- Reduce(intersect, lapply(a, function(x) x$x$SNP))
+	a <- lapply(a, function(x)
+	{
+		index1 <- which(x$x$SNP %in% snps)
+		x$x <- x$x[index1,]
+		if(ldflag)
+		{
+			x$ld <- x$ld[index1, index1]
+		}
+		return(x)
+	})
+	if(length(ids) > 1)
+	{
+		stopifnot(all(a[[1]]$x$SNP == a[[2]]$x$SNP))
+	}
+	return(a)  	
+  },
+
+  # to build on extract_instrument_regions we can do finemapping
+  # If we want to do fine mapping we need to get an LD matrix for the whole region (for each population)
+  # We then need to harmonise the LD matrix to the summary data, and the summary datasets to each other
+  regional_ld_matrices = function(),
+
+  # for each instrument region + ld matrix we can perform susie finemapping
+  # do this independently in each population
+  # find credible sets that overlap - to try to determine the best SNP in a region to be used as instrument
+  susie_finemap_regions = function(),
+
+  # PAINTOR allows finemapping jointly across multiple populations
+  # returns a posterior probability of inclusion for each SNP
+  # Choose the variant with highest posterior probability and associations in each exposure
+  paintor_finemap_regions = function(),
+
+  # Once a set of instruments is chosen we can ask
+  # - what fraction of those primarily identified in pop1 replicate in pop2?
+  # - what fraction of those primarily identified in pop1 have the same sign as in pop2?
+  # - compare these to what is expected by chance under the hypothesis that the effect estimates are the same
+  # this will provide some evidence for whether lack of replication is due to (e.g.) GxE
+  proportion_instrument_overlap = function(b_disc, b_rep, se_disc, se_rep, alpha)
+  {
+	p_sign <- pnorm(-abs(b_disc) / se_disc) * pnorm(-abs(b_disc) / se_rep) + ((1 - pnorm(-abs(b_disc) / se_disc)) * (1 - pnorm(-abs(b_disc) / se_rep)))
+	p_sig <- pnorm(-abs(b_disc) / se_disc + qnorm(alpha / 2)) + (1 - pnorm(-abs(b_disc) / se_disc - qnorm(alpha / 2)))
+	p_rep <- pnorm(abs(b_rep)/se_rep, lower.tail=FALSE)
+	message("Total: ", length(b_disc))
+	message("Expected sign: ", sum(p_sign, na.rm=TRUE))
+	message("Observed sign: ", sum(sign(b_disc) == sign(b_rep)))
+	message("Expected sig: ", sum(p_sig, na.rm=TRUE))
+	message("Observed sig: ", sum(p_rep < alpha, na.rm=TRUE))
+	return(list(sum(p_sign, na.rm=TRUE), sum(p_sig, na.rm=TRUE), data_frame(sig=p_sig, sign=p_sign)))
+  },
+
+  # Once a set of instruments is defined then extract the outcome data
+  # Could use 
+  # - raw results from extract_instruments
+  # - maximised associations from extract_instrument_regions
+  # - finemapped hits from susie_finemap_regions
+  # - finemapped hits from paintor_finemap_regions
+  extract_outcome_data = function(),
+
+  # Perform basic SEM analysis of the joint estimates in multiple ancestries
+  perform_basic_sem = function()
+
+
+  # Plots
+  # Plot regional associations for each instrument and each population
+  # Plot finemapping results against regional association data
+
+))
+
+
+
+
+
+
+
+
 
 susie_overlaps <- function(su1, su2)
 {
@@ -215,77 +362,116 @@ plot_region <- function(region)
 		facet_grid(id.exposure ~ .)
 }
 
+#' Analyse regional data with PAINTOR
+#'
+#' @param regiondata Output from extract_regional_data
+#' @param PAINTOR Path to PAINTOR executable. Default="PAINTOR" 
+#' @param workdir Working directory. Default=tempdir()
+#'
+#' @export
+#' @return Results table with posterior inclusion probabilities
+run_PAINTOR <- function(regiondata, PAINTOR="PAINTOR", workdir=tempdir())
+{
+	nid <- length(regiondata)
+	snps <- regiondata[[1]]$x$SNP
+	zs <- lapply(1:nid, function(i)
+	{
+		o <- list()
+		o[[paste0("ZSCORE.P", i)]] <- regiondata[[i]]$x$beta.exposure / regiondata[[i]]$x$se.exposure
+		return(as_tibble(o))
+	}) %>% bind_cols()
+
+	locus <- tibble(CHR = regiondata[[1]]$x$chr.exposure, POS = regiondata[[1]]$x$position.exposure, RSID = regiondata[[1]]$x$SNP) %>%
+		bind_cols(., zs)
+	write.table(locus, file=file.path(workdir, "Locus1"), row=F, col=T, qu=F)
+	lapply(1:nid, function(i)
+	{
+		write.table(regiondata[[i]]$ld, file=file.path(workdir, paste0("Locus1.LD", i)), row=FALSE, col=FALSE, qu=FALSE)
+	})
+	write.table(tibble(null=rep(1, length(snps))), file=file.path(workdir, "Locus1.annotations"), row=F, col=T, qu=F)
+
+	write.table(c("Locus1"), file=file.path(workdir, "input.files"), row=F, col=F, qu=F)
+
+	LDname <- paste(paste0("LD", 1:nid), collapse=",")
+	Zhead <- paste(names(zs), collapse=',')
+	glue("{PAINTOR} -input {file.path(workdir,'input.files')} -Zhead {Zhead} -LDname {LDname} -in {workdir} -out {workdir} -mcmc -annotations null") %>%
+		system()
+	res <- data.table::fread(file.path(workdir, "Locus1.results"))
+	unlink(workdir)
+	return(res)
+}
 
 
+#' Plot PAINTOR results
+#'
+#' @param res Output from run_PAINTOR
+#'
+#' @export
+#' @return plot
+plot_PAINTOR <- function(res)
+{
+	res %>% pivot_longer(cols=c(ZSCORE.P1, ZSCORE.P2, Posterior_Prob)) %>%
+		ggplot(., aes(x=POS, y=value)) +
+		geom_point(aes(colour=name)) +
+		facet_grid(name ~ ., scale="free_y")
+}
 
-radius=50000
-id1 <- "bbj-a-52"
-id2 <- "ukb-b-20175"
-bfile1="/Users/gh13047/data/ld_files/ldmat/EAS"
-bfile2="/Users/gh13047/data/ld_files/ldmat/EUR"
-plink="plink"
-alpha=0.05
-mt_method="fdr"
-ld_thresh=0.05
-prune_bfile=NULL
-prune_pop="EUR"
+#' Analyse regional data with MsCAVIAR
+#'
+#' @param regiondata Output from extract_regional_data
+#' @param MsCAVIAR Path to MsCAVIAR executable. Default="MsCAVIAR" 
+#' @param workdir Working directory. Default=tempdir()
+#'
+#' @export
+#' @return Results table with posterior inclusion probabilities
+run_MsCAVIAR <- function(regiondata, n, MsCAVIAR="MsCAVIAR", workdir=tempdir())
+{
+	nid <- length(regiondata)
+	snps <- regiondata[[1]]$x$SNP
+	lapply(1:nid, function(i)
+	{
+		regiondata[[i]]$x %>% 
+			dplyr::mutate(z=beta.exposure/se.exposure) %>%
+			dplyr::select(SNP, z) %>%
+			write.table(., file=file.path(workdir, paste0("z.", i)), row=F, col=F, qu=F)
+	})
 
-inst <- trans_mr_instruments(id1, id2, radius, plink=plink, bfile1=bfile1, bfile2=bfile2, ld_thresh=ld_thresh, prune_bfile=bfile1)
+	lapply(1:nid, function(i)
+	{
+		write.table(regiondata[[i]]$ld, file=file.path(workdir, paste0("ld.", i)), row=FALSE, col=FALSE, qu=FALSE)
+	})
 
+	thisdir <- getwd()
+	setwd(workdir)
+	write.table(paste0("ld.", 1:nid), file="ldfiles.txt", row=F, col=F, qu=F)
+	write.table(paste0("z.", 1:nid), file="zfiles.txt", row=F, col=F, qu=F)
+	glue("{MsCAVIAR} -l ldfiles.txt -z zfiles.txt -n {paste(n, collapse=',')} -o results") %>% system()
+	res <- data.table::fread("results_post.txt")
+	zs <- lapply(1:nid, function(i)
+	{
+		out <- list()
+		out[[paste0("ZSCORE.P", i)]] <- regiondata[[i]]$x$beta.exposure / regiondata[[i]]$x$se.exposure
+		as_tibble(out)
+	}) %>% bind_cols()
+	stopifnot(all(res$SNP_ID == regiondata[[1]]$x$SNP))
+	names(res)[1] <- c("SNP")
+	res <- bind_cols(res, zs) %>%
+		mutate(CHR = regiondata[[1]]$x$chr.exposure, POS=regiondata[[1]]$x$position.exposure)
+	setwd(thisdir)
+	return(res)
+}
 
-d <- associations(inst$res$bestsnp[!is.na(inst$res$bestsnp)], args$ids)
-d$padj <- p.adjust(d$p, "fdr")
-dplyr::select(d, rsid, id, padj) %>%
-	dplyr::group_by(rsid, id) %>%
-	dplyr::slice(1) %>%
-	pivot_wider(names_from=id, values_from=padj) %>%
-	ungroup() %>%
-	summarise(s1 = sum(`bbj-a-52` < 0.05 & `ukb-b-20175` > 0.5), s2 = sum(`bbj-a-52` > 0.5 & `ukb-b-20175` < 0.05), sb = sum(`bbj-a-52` < 0.05 & `ukb-b-20175` < 0.05))
-
-inst$inst %>%
-	mutate(padj = p.adjust(pval.exposure, "fdr")) %>%
-	dplyr::select(rsid=SNP, id=id.exposure, padj) %>%
-	dplyr::group_by(rsid, id) %>%
-	dplyr::slice(1) %>%
-	pivot_wider(names_from=id, values_from=padj) %>%
-	ungroup() %>%
-	summarise(s1 = sum(`bbj-a-52` < 0.05 & `ukb-b-20175` > 0.5), s2 = sum(`bbj-a-52` > 0.5 & `ukb-b-20175` < 0.05), sb = sum(`bbj-a-52` < 0.05 & `ukb-b-20175` < 0.05))
-
-table(d$rsid %in% inst$inst$SNP)
-
-d3 <- independent_instruments(args$ids[1], args$ids[2])
-
-d4 <- associations(d3$rsid, args$ids)
-d4$padj <- p.adjust(d4$p, "fdr")
-dplyr::select(d4, rsid, id, padj) %>%
-	dplyr::group_by(rsid, id) %>%
-	dplyr::slice(1) %>%
-	pivot_wider(names_from=id, values_from=padj) %>%
-	ungroup() %>%
-	summarise(s1 = sum(`bbj-a-52` < 0.05 & `ukb-b-20175` > 0.5), s2 = sum(`bbj-a-52` > 0.5 & `ukb-b-20175` < 0.05), sb = sum(`bbj-a-52` < 0.05 & `ukb-b-20175` < 0.05))
-
-
-
-inst$res
-
-which(inst$res$cs_overlap)
-
-
-plot_region(inst$info[[100]])
-susie_plot(inst$info[[100]]$su1, "PIP")
-dev.new()
-susie_plot(inst$info[[100]]$su2, "PIP")
-
-table(inst$res$cs_overlap)
-
-dim(inst$res)
-table(inst$res$type)
-
-saveRDS(inst, file="sbp_eas_eur.rds")
-
-plot_region(l[[200]])
-plot_region(l[[207]])
-
-susie_plot(l[[2]]$su1, "PIP")
-susie_plot(l[[2]]$su2, "PIP")
+#' Plot MsCAVIAR results
+#'
+#' @param res Output from run_MsCAVIAR
+#'
+#' @export
+#' @return plot
+plot_MsCAVIAR <- function(res)
+{
+	res %>% pivot_longer(cols=c(ZSCORE.P1, ZSCORE.P2, Prob_in_pCausalSet)) %>%
+		ggplot(., aes(x=POS, y=value)) +
+		geom_point(aes(colour=name)) +
+		facet_grid(name ~ ., scale="free_y")
+}
 
