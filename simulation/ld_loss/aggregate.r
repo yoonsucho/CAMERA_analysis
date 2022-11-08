606ce0808d58c3d969ca039f402a7a840ebe25fb
Gibran Hemani <explodecomputer@gmail.com>
2022-11-08 13:35:24 +0000

    first batch of results

---
+++ simulation/ld_loss/aggregate.r
@@ -0,0 +1,15 @@
+library(dplyr)
+library(jsonlite)
+library(here)
+config <- read_json(here("simulation/ld_loss/config.json"))
+a <- list.files(config$outdir, pattern="result.rdata", recursive=TRUE)
+length(a)
+
+o <- lapply(a, function(f){
+    load(file.path(config$outdir, f))
+    return(list(snplist, res))
+})
+
+res <- lapply(o, function(x) x[[2]]) %>% bind_rows()
+snplist <- lapply(o, function(x) x[[1]]) %>% bind_rows()
+save(res, snplist, file=file.path(config$outdir, "results.rdata"))
